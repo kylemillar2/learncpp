15e68e4f527cf5ddd6694640180c7211f1c613c9
Kyle Millar <kyledmillar@gmail.com>
2024-12-24 17:44:45 +0000

    Initial commit

---
+++ chapter1/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Enter three numbers: ";
+    int x{};
+    int y{};
+    int z{};
+    std::cin >> x >> y >> z;
+    std::cout << "You entered " << x << ", " << y << ", and " << z << std::endl;
+
+    return 0;
+}
